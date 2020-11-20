6096ff4b0e2a4729575158c24c84e1add005477b
Idriss Neumann <idriss.neumann@comwork.io>
2020-11-20 21:35:38 +0100

    Add init-github pipeline composite task

---
+++ init-pipeline-github/README.md
@@ -0,0 +1,18 @@
+# Init a pipeline from github
+
+Composite task that will use:
+
+* [`github-search-pr`](./github-search-pr)
+* [`github-set-status`](./github-set-status)
+* [`github-add-comment`](./github-add-comment)
+* [`slack-sender`](./slack-sender)
+* [`git-fetch-sources`](./git-fetch-sources)
+
+## Environment variables to set
+
+* `LOG_LEVEL`: log level (`debug` or `info`)
+* `TEKTON_WORKSPACE_PATH`: the tekton workspace path which is bind to a pvc
+* `PIPELINE_URL`: pipeline url
+* `REPO_ORG`: github repo org
+* `REPO_NAME`: github repo name
+* `GIT_BRANCH`: git branch name
---
+++ init-pipeline-github/init-pipeline.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export SLACK_COLOR="#87CEEB"
+export SLACK_USERNAME="tekton"
+export SLACK_EMOJI_AVATAR=":tekton:"
+export SLACK_MSG="Pipeline in progress, follow here: ${PIPELINE_URL}"
+export COMMENT="${SLACK_MSG}"
+export PIPELINE_TARGET_URL="${PIPELINE_URL}"
+export DEFAULT_PIPELINE_STATE="pending"
+[[ ! $STATUS_DESCRIPTION ]] && export STATUS_DESCRIPTION="build in progress"
+[[ ! $STATUS_CONTEXT ]] && export STATUS_CONTEXT="continuous-integration/tekton"
+
+/bin/sh /search.sh
+python3 set-status.py
+python3 slack-sender.py
+python3 add-comment.py
+/bin/sh fetch-sources.sh
