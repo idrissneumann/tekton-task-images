854b6f6f4265665b7230732df66d513f78c8502f
Idriss Neumann <idriss.neumann@comwork.io>
2020-11-21 00:12:12 +0100

    POSIX compliance

--- init-pipeline-github/init-pipeline.sh
+++ init-pipeline-github/init-pipeline.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 export SLACK_COLOR="#87CEEB"
 export SLACK_USERNAME="tekton"
@@ -7,11 +7,11 @@ export SLACK_MSG="Pipeline in progress, follow here: ${PIPELINE_URL}"
 export COMMENT="${SLACK_MSG}"
 export PIPELINE_TARGET_URL="${PIPELINE_URL}"
 export DEFAULT_PIPELINE_STATE="pending"
-[[ ! $STATUS_DESCRIPTION ]] && export STATUS_DESCRIPTION="build in progress"
-[[ ! $STATUS_CONTEXT ]] && export STATUS_CONTEXT="continuous-integration/tekton"
+[ -z "${STATUS_DESCRIPTION}" ] && export STATUS_DESCRIPTION="build in progress"
+[ -z "${STATUS_CONTEXT}" ] && export STATUS_CONTEXT="continuous-integration/tekton"
 
 /bin/sh /search.sh
-python3 set-status.py
-python3 slack-sender.py
-python3 add-comment.py
-/bin/sh fetch-sources.sh
+python3 /set-status.py
+python3 /slack-sender.py
+python3 /add-comment.py
+/bin/sh /fetch-sources.sh
