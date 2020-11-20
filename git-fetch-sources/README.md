9bf2ae791dfcf373bd4a601bfa47fd295e2ac229
Idriss Neumann <idriss.neumann@comwork.io>
2020-11-20 19:32:43 +0100

    Add fetch sources task

---
+++ git-fetch-sources/README.md
@@ -0,0 +1,11 @@
+# Git fetch sources
+
+Fetch the up to date sources and init environment variables for the other tasks.
+
+## Environment variables to set
+
+* `LOG_LEVEL`: log level (`debug` or `info`)
+* `TEKTON_WORKSPACE_PATH`: the tekton workspace path which is bind to a pvc
+* `REPO_ORG`: github repo org
+* `REPO_NAME`: github repo name
+* `GIT_BRANCH`: git branch name
---
+++ git-fetch-sources/fetch-sources.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+suffix=$(echo ${HOSTNAME}|sed 's/\(.*\)-.*/\1/')
+pr_env_file="${TEKTON_WORKSPACE_PATH}/pr_env-${suffix}.sh"
+git_script="${TEKTON_WORKSPACE_PATH}/git.sh"
+env_script="${TEKTON_WORKSPACE_PATH}/env.sh"
+log_script="${TEKTON_WORKSPACE_PATH}/log.sh"
+
+[[ -f $pr_env_file ]] && source "$pr_env_file" 
+
+echo "export LOG_LEVEL=${LOG_LEVEL}" > "${env_script}"
+echo "export GIT_BRANCH=${GIT_BRANCH}" >> "${env_script}"
+echo "export REPO_ORG=${REPO_ORG}" >> "${env_script}"
+echo "export REPO_NAME=${REPO_NAME}" >> "${env_script}"
+echo "export WORKING_DIR=/workspace/git-\$REPO_ORG-\$REPO_NAME" >> "${env_script}"
+source "${env_script}"
+echo "source env.sh" > "${log_script}"
+
+if [[ $LOG_LEVEL == "debug" || $LOG_LEVEL == "DEBUG" ]]; then
+  echo "echo '[init][debug] Environment variables values'"  >> "${log_script}"
+  echo 'env|grep -iv token|grep -iv passwd|grep -iv password' >> "${log_script}"
+  echo "echo '[init][debug] Content of the current working dir'"  >> "${log_script}"
+  echo 'ls -la' >> "${log_script}"
+fi
+
+echo "source env.sh" > "${git_script}"
+[[ $LOG_LEVEL == "debug" || $LOG_LEVEL == "DEBUG" ]] && echo "echo '[git][1/4] installation'" >> "${git_script}"
+echo "apk add --no-cache git" >> "${git_script}"
+[[ $LOG_LEVEL == "debug" || $LOG_LEVEL == "DEBUG" ]] && echo "echo '[git][2/4] pull'" >> "${git_script}"
+echo "git pull" >> "${git_script}"
+[[ $LOG_LEVEL == "debug" || $LOG_LEVEL == "DEBUG" ]] && echo "echo '[git][3/4] checkout '\$GIT_BRANCH" >> "${git_script}"
+echo "git checkout \$GIT_BRANCH" >> "${git_script}"
+[[ $LOG_LEVEL == "debug" || $LOG_LEVEL == "DEBUG" ]] && echo "echo '[git][4/4] pull rebase '\$GIT_BRANCH" >> "${git_script}"
+echo "git pull --rebase origin \$GIT_BRANCH" >> "${git_script}"
+
+/bin/sh "${log_script}"
+/bin/sh "${git_script}"
+
+target_path="${TEKTON_WORKSPACE_PATH}"
+ls -a . |while read; do [[ $REPLY != "." && $REPLY != ".." ]] && cp -R $REPLY $target_path/; done
+if [[ $LOG_LEVEL == "debug" || $LOG_LEVEL == "DEBUG" ]]; then
+  echo "[github-fetch-sources][debug] Content of workspaces.path = ${target_path}"
+  ls -la "${target_path}"
+fi
