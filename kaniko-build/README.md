5105937be3a3807b6ccc3cadc35277d84c328ae6
Idriss Neumann <idriss.neumann@comwork.io>
2020-11-21 08:13:03 +0100

    Add an example for Kaniko build

--- kaniko-build/README.md
+++ kaniko-build/README.md
@@ -15,4 +15,7 @@ Build containers images with Kaniko.
 * `EXTRA_ARGS`: extra args (multistage targets for example)
 * `DOCKERFILE`: path to the Dockerfile
 * `CONTEXT`: the build context used by Kaniko
+* `PROJECT_STABLE`: path of stables images in the registry (after merge)
+* `PROJECT_UNSTABLE`: path of stables images in the registry (before merge)
+* `FORCE_PROJECT_UNSTABLE`: force push in the `$PROJECT_UNSTABLE` path
 * `TEKTON_WORKSPACE_PATH`: the tekton workspace path which is bind to a pvc
--- kaniko-build/build.sh
+++ kaniko-build/build.sh
@@ -8,8 +8,8 @@ if [ "${LOG_LEVEL}" == "debug" ] || [ "${LOG_LEVEL}" == "DEBUG" ]; then
 fi
 
 DOCKER_REGISTRY_ORG=$PROJECT_STABLE
-if [ "${GIT_BRANCH}" != "develop" ] && [ "${GIT_BRANCH}" != "main" ] && [ "${GIT_BRANCH}" != "master" ] && ! echo "${GIT_BRANCH}"|grep -E "^[0-9]+.[0-9]+.x$" > /dev/null || [ "${FORCE_PROJECT_CI}" == "enabled" ]; then
-    DOCKER_REGISTRY_ORG=$PROJECT_CI
+if [ "${GIT_BRANCH}" != "develop" ] && [ "${GIT_BRANCH}" != "main" ] && [ "${GIT_BRANCH}" != "master" ] && ! echo "${GIT_BRANCH}"|grep -E "^[0-9]+.[0-9]+.x$" > /dev/null || [ "${FORCE_PROJECT_UNSTABLE}" == "enabled" ]; then
+    DOCKER_REGISTRY_ORG=$PROJECT_UNSTABLE
 fi
 
 IMAGE="${DOCKER_REGISTRY}/${DOCKER_REGISTRY_ORG}/${IMAGE}:${IMAGE_TAG}"
@@ -22,7 +22,7 @@ fi
 
 CACHE_OPTS=""
 if [ "${ENABLE_CACHE}" == "enabled" ] || [ "${ENABLE_CACHE}" == "ok" ] || [ "${ENABLE_CACHE}" == "true" ] || [ "${ENABLE_CACHE}" == "enabled" ]; then
-    CACHE_OPTS="--cache=true --cache-ttl=${CACHE_TTL} --cache-repo=${DOCKER_REGISTRY}/${PROJECT_CI}/cache --cache-dir=$TEKTON_WORKSPACE_PATH"
+    CACHE_OPTS="--cache=true --cache-ttl=${CACHE_TTL} --cache-repo=${DOCKER_REGISTRY}/${PROJECT_UNSTABLE}/cache --cache-dir=$TEKTON_WORKSPACE_PATH"
 fi
 
 /kaniko/executor ${EXTRA_ARGS} $CACHE_OPTS --dockerfile=$DOCKERFILE --context=$TEKTON_WORKSPACE_PATH/$CONTEXT --destination=$IMAGE --oci-layout-path=$TEKTON_WORKSPACE_PATH/$CONTEXT/image-digest
