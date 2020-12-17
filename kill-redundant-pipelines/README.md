0af2acd866656ec2e81db2a4c0db78448eea5b45
Idriss Neumann <idriss.neumann@comwork.io>
2020-12-17 18:34:53 +0100

    Be able to destroy the associated pvc

--- kill-redundant-pipelines/README.md
+++ kill-redundant-pipelines/README.md
@@ -8,6 +8,7 @@ Check if a pipeline is already running on the pull request.
 * `TEKTON_NAMESPACE`: namespace of tekton pipelines
 * `PROJECT_NAME`: project name (prefix of the pipelineruns name)
 * `BUILD_ID`: build id (suffix of the pipelineruns name) to filter
+* `DESTROY_PVC`: destroy an associated pvc (`disabled` or `enabled`)
 * `KUBE_ENV`: kubernetes env name
 * `KUBE_URL`: kubernetes cluster url
 * `KUBE_TOKEN`: kubernetes token
--- kill-redundant-pipelines/kill-redundant-pipelines.sh
+++ kill-redundant-pipelines/kill-redundant-pipelines.sh
@@ -2,6 +2,7 @@
 
 [ -z "${TEKTON_NAMESPACE}" ] && TEKTON_NAMESPACE="tekton-pipelines"
 [ -z "${BUILD_ID}" ] && BUILD_ID="not_mandatory"
+[ -z "${DESTROY_PVC}" ] && DESTROY_PVC="disabled"
 
 kubectl -n "${TEKTON_NAMESPACE}" get pipelineruns|awk '{if ($2 = "Unknown" && $3 ~ "Running" && $1 ~ "'"${PROJECT_NAME}"'.*" && !($1 ~ ".*'"${BUILD_ID}"'")){print $1}}'|while read pipeline; do
     [ "${LOG_LEVEL}" = "debug" ] || [ "${LOG_LEVEL}" = "DEBUG" ] && echo "[kill-redundant-pipelines][debug] Checking branch for ${pipeline}..."
@@ -10,5 +11,10 @@ kubectl -n "${TEKTON_NAMESPACE}" get pipelineruns|awk '{if ($2 = "Unknown" && $3
     if [ "${branch}" = "${GIT_BRANCH}" ]; then
         echo "[kill-redundant-pipelines] Killing ${pipeline} because branch=${branch}..."
         kubectl -n "${TEKTON_NAMESPACE}" delete pipelineruns "${pipeline}"
+        if [ "${DESTROY_PVC}" = "enabled" ]; then
+            PVC=$(echo "${pipeline}"|sed "s/${PROJECT_NAME}/${PROJECT_NAME}-pvc/g")
+            echo "[kill-redundant-pipelines] Removing pvc ${PVC}..."
+            kubectl -n "${TEKTON_NAMESPACE}" delete pvc "${PVC}"
+        fi
     fi
 done
