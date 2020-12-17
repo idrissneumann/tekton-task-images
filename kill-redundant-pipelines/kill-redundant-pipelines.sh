9b7c69fbf38c9a6c38e620ca2e1c29b4fc4092fd
Idriss Neumann <idriss.neumann@comwork.io>
2020-12-17 20:34:33 +0100

    Fix: use the right kubeconfig

--- kill-redundant-pipelines/kill-redundant-pipelines.sh
+++ kill-redundant-pipelines/kill-redundant-pipelines.sh
@@ -4,6 +4,8 @@
 [ -z "${BUILD_ID}" ] && BUILD_ID="not_mandatory"
 [ -z "${DESTROY_PVC}" ] && DESTROY_PVC="disabled"
 
+/init-kube-config.sh
+
 kubectl -n "${TEKTON_NAMESPACE}" get pipelineruns|awk '{if ($2 = "Unknown" && $3 ~ "Running" && $1 ~ "'"${PROJECT_NAME}"'.*" && !($1 ~ ".*'"${BUILD_ID}"'")){print $1}}'|while read pipeline; do
     [ "${LOG_LEVEL}" = "debug" ] || [ "${LOG_LEVEL}" = "DEBUG" ] && echo "[kill-redundant-pipelines][debug] Checking branch for ${pipeline}..."
     branch=$(kubectl -n "${TEKTON_NAMESPACE}" get pipelineruns "${pipeline}" -o json|jq -r '.spec.params | map(select(.name ==  "gitBranchName")) | .[] .value')
@@ -18,3 +20,5 @@ kubectl -n "${TEKTON_NAMESPACE}" get pipelineruns|awk '{if ($2 = "Unknown" && $3
         fi
     fi
 done
+
+[[ -f ~/.kube/config.old ]] && cp -f ~/.kube/config.old ~/.kube/config
