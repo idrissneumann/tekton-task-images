b85871d120ffbc63cb890b172607fbea293b1fb2
Idriss Neumann <idriss.neumann@comwork.io>
2020-12-18 16:52:36 +0100

    Typo parameter username

--- end-pipeline-github/end-pipeline.sh
+++ end-pipeline-github/end-pipeline.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-[ -z "${PIPELINE_TARGET_URL}" ] && PIPELINE_TARGET_URL="${PIPELINE_URL}"
+[ -z "${PIPELINE_TARGET_URL}" ] && export PIPELINE_TARGET_URL="${PIPELINE_URL}"
 
 /set-status.py
 /slack-result-sender.py
