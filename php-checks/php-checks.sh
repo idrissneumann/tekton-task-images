cf718976bbff404b0638659f368de8b92b3d1243
Idriss Neumann <idriss.neumann@comwork.io>
2020-12-18 19:40:45 +0100

    Fix missing blank and return code

--- php-checks/php-checks.sh
+++ php-checks/php-checks.sh
@@ -5,10 +5,10 @@ cd "${TEKTON_WORKSPACE_PATH}"
 [ -z "${SRC_DIR}" ] && SRC_DIR="src/"
 [ -z "${PSALM_FILE}" ] && PSALM_FILE="psalm.xml"
 [ -z "${INSTALL_EXTRA_ARGS}" ] && INSTALL_EXTRA_ARGS="--prefer-dist --no-progress --no-suggest"
-[ -z "${ENABLE_COMPOSER_INSTALL}"] && ENABLE_COMPOSER_INSTALL="disabled"
-[ -z "${ENABLE_PHPCS}"] && ENABLE_PHPCS="disabled"
-[ -z "${ENABLE_PSALM}"] && ENABLE_PSALM="disabled"
-[ -z "${ENABLE_PHPUNIT}"] && ENABLE_PHPUNIT="disabled"
+[ -z "${ENABLE_COMPOSER_INSTALL}" ] && ENABLE_COMPOSER_INSTALL="disabled"
+[ -z "${ENABLE_PHPCS}" ] && ENABLE_PHPCS="disabled"
+[ -z "${ENABLE_PSALM}" ] && ENABLE_PSALM="disabled"
+[ -z "${ENABLE_PHPUNIT}" ] && ENABLE_PHPUNIT="disabled"
  
 if [ "${LOG_LEVEL}" = "debug" ] || [ "${LOG_LEVEL}" = "DEBUG" ]; then
   echo "[php-ci][debug] Show the occuped space of workspace"
@@ -16,23 +16,29 @@ if [ "${LOG_LEVEL}" = "debug" ] || [ "${LOG_LEVEL}" = "DEBUG" ]; then
   echo "[php-ci][debug] SRC_DIR=${SRC_DIR}, ENABLE_COMPOSER_INSTALL=${ENABLE_COMPOSER_INSTALL}, ENABLE_PHPCS=${ENABLE_PHPCS}, ENABLE_PSALM=${ENABLE_PSALM}, ENABLE_PHPUNIT=${ENABLE_PHPUNIT}"
 fi
 
-
-if [ "${ENABLE_COMPOSER_INSTALL}" == "enabled" ]; then
+rtn=0
+if [ "${ENABLE_COMPOSER_INSTALL}" = "enabled" ]; then
     echo "[php-ci] run composer install ${INSTALL_EXTRA_ARGS}"
     composer install ${INSTALL_EXTRA_ARGS}
+    rtn=$?
 fi
 
-if [ "${ENABLE_PHPCS}" == "enabled" ]; then
+if [ $rtn -eq 0 ] && [ "${ENABLE_PHPCS}" = "enabled" ]; then
     echo "[php-ci] run ./vendor/bin/phpcs ${SRC_DIR}"
     ./vendor/bin/phpcs ${SRC_DIR}
+    rtn=$?
 fi
 
-if [ "${ENABLE_PSALM}" == "enabled" ]; then
+if [ $rtn -eq 0 ] && [ "${ENABLE_PSALM}" = "enabled" ]; then
     echo "[php-ci] run ./vendor/bin/psalm -c=${PSALM_FILE}"
     ./vendor/bin/psalm -c=${PSALM_FILE}
+    rtn=$?
 fi
 
-if [ "${ENABLE_PHPUNIT}" == "enabled" ]; then
+if [ $rtn -eq 0 ] && [ "${ENABLE_PHPUNIT}" = "enabled" ]; then
     echo "[php-ci] run ./vendor/bin/phpunit"
     ./vendor/bin/phpunit
+    rtn=$?
 fi
+
+exit $rtn
--- yarn-checks/yarn-checks.sh
+++ yarn-checks/yarn-checks.sh
@@ -9,15 +9,20 @@ if [ "${LOG_LEVEL}" = "debug" ] || [ "${LOG_LEVEL}" = "DEBUG" ]; then
   du -sh "${TEKTON_WORKSPACE_PATH}"
 fi
 
+rtn=0
 if [ "${YARN_INSTALL}" = "enabled" ]; then
   yarn install $INSTALL_EXTRA_OPT
+  rtn=$?
 fi
 
-if [ "${LOG_LEVEL}" = "debug" ] || [ "${LOG_LEVEL}" = "DEBUG" ]; then
+if [ $rtn -eq 0 ] && [ "${LOG_LEVEL}" = "debug" ] || [ "${LOG_LEVEL}" = "DEBUG" ]; then
   echo "[yarn-ci][success][debug] Show the occuped space of workspace"
   du -sh "${TEKTON_WORKSPACE_PATH}"
 fi
 
-if [ ! -z "${TARGET}" ] && [ "${TARGET}" != "only_install" ]; then
+if [ $rtn -eq 0 ] && [ ! -z "${TARGET}" ] && [ "${TARGET}" != "only_install" ]; then
   yarn $TARGET
+  rtn=$?
 fi
+
+exit $rtn
