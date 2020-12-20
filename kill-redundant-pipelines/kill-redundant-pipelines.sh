7b166d437276d0957126f8770b378bc532e0fbe9
Idriss Neumann <idriss.neumann@comwork.io>
2020-12-20 16:46:14 +0100

    Cancel pipelinerun instead of delete

--- kill-redundant-pipelines/kill-redundant-pipelines.sh
+++ kill-redundant-pipelines/kill-redundant-pipelines.sh
@@ -15,7 +15,7 @@ kubectl -n "${TEKTON_NAMESPACE}" get pipelineruns|awk '{if ($2 = "Unknown" && $3
     
   if [ "${branch}" = "${GIT_BRANCH}" ]; then
     echo "[kill-redundant-pipelines] Killing ${pipeline} because branch=${branch}..."
-    kubectl -n "${TEKTON_NAMESPACE}" delete pipelineruns "${pipeline}"
+    tkn -n "${TEKTON_NAMESPACE}" pipelinerun cancel "${pipeline}"
     if [ "${DESTROY_PVC}" = "enabled" ]; then
       PVC=$(echo "${pipeline}"|sed "s/${PROJECT_NAME}/${PROJECT_NAME}-pvc/g")
       echo "[kill-redundant-pipelines] Removing pvc ${PVC}..."
