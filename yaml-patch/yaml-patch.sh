40267dd0a4113e20871eb0fc00d7442a8d478f30
Idriss Neumann <idriss.neumann@comwork.io>
2021-01-12 14:41:04 +0100

    Fix: remove old tar gz

--- yaml-patch/yaml-patch.sh
+++ yaml-patch/yaml-patch.sh
@@ -4,7 +4,7 @@ INPUT_FILE="${TEKTON_WORKSPACE_PATH}/${YAML_INPUT_FILE_PATH}"
 
 echo "[yaml-patch] patching ${INPUT_FILE} with ${YQ_EXPRESSION}"
 
-yq e "${YQ_EXPRESSION}" "${INPUT_FILE}" | sponge "${INPUT_FILE}"
+yq e "${YQ_EXPRESSION}" -i "${INPUT_FILE}"
 
 if [ "${LOG_LEVEL}" = "debug" ] || [ "${LOG_LEVEL}" = "DEBUG" ]; then
     echo "[yaml-patch][debug] content of ${INPUT_FILE} after patch:"
