cbd8c44ceb984967a722d5942afdff3c93b20b57
Idriss Neumann <idriss.neumann@comwork.io>
2021-01-14 16:50:15 +0100

    Fix: failfast if the directory doesn't exists

--- github-bump-pr/fetch.sh
+++ github-bump-pr/fetch.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-[[ ! -d "${GIT_WORKSPACE_PATH}" ]] && export GIT_WORKSPACE_PATH="/workspaces/gitops-input"
-[[ ! -d "${GIT_WORKSPACE_PATH}" ]] && export GIT_WORKSPACE_PATH="/workspaces/git-${REPO_ORG}-${REPO_NAME}-${GIT_SRC_BRANCH}"
-[[ ! -d "${GIT_WORKSPACE_PATH}" ]] && export GIT_WORKSPACE_PATH="/workspaces/git-${REPO_ORG}-${REPO_NAME}"
+[[ ! -d "${ROOT_WORKSPACE_DIR}" ]] && export ROOT_WORKSPACE_DIR="/workspace"
+if [[ ! -d "${ROOT_WORKSPACE_DIR}" ]]; then
+  echo "[github-bump-pr][fetch.sh] ROOT_WORKSPACE_DIR=${ROOT_WORKSPACE_DIR} doesn't exists as a directory"
+  exit 1
+fi
+
+[[ ! -d "${GIT_WORKSPACE_PATH}" ]] && export GIT_WORKSPACE_PATH="${ROOT_WORKSPACE_DIR}/git-${REPO_ORG}-${REPO_NAME}-${GIT_SRC_BRANCH}"
+[[ ! -d "${GIT_WORKSPACE_PATH}" ]] && export GIT_WORKSPACE_PATH="${ROOT_WORKSPACE_DIR}/git-${REPO_ORG}-${REPO_NAME}"
+[[ ! -d "${GIT_WORKSPACE_PATH}" ]] && export GIT_WORKSPACE_PATH="${ROOT_WORKSPACE_DIR}/gitops-input"
 
 if [[ ! -d "${GIT_WORKSPACE_PATH}" ]]; then
   echo "[github-bump-pr][fetch.sh] GIT_WORKSPACE_PATH=${GIT_WORKSPACE_PATH} doesn't exists as a directory"
