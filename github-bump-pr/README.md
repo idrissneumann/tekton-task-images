626dafc9f9743e6b904c0ab2d65f46e2e25a752c
Idriss Neumann <idriss.neumann@comwork.io>
2021-01-14 08:14:43 +0100

    Add a version pattern to replace

--- github-bump-pr/README.md
+++ github-bump-pr/README.md
@@ -11,7 +11,7 @@ Open a bump pull request
 * `REPO_ORG`: github repo org
 * `REPO_NAME`: github repo name
 * `YAML_INPUT_FILE_PATH`: relative path to the input yaml file (in the `TEKTON_WORKSPACE_PATH`)
-* `YQ_EXPRESSION`: `yq` patch expression
+* `YQ_EXPRESSION`: `yq` patch expression (it can contain `VERSION_TO_REPLACE` that will be automatically replaced by the git tag version)
 * `GIT_TARGET_BRANCH`: git target branch of the deployment/gitops repository (default: `master`)
 
 For multiple expressions and multiple file, you can use `YQ_EXPRESSION_X` with `YAML_INPUT_FILE_PATH_X` instead of `YAML_INPUT_FILE_PATH` and `YQ_EXPRESSION`, with `X`, a number that need to match for both variables.
--- github-bump-pr/bump.sh
+++ github-bump-pr/bump.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+replace_version() {
+  yq_expr_full_var="${1}"
+  yq_expr_var="$(echo ${yq_expr_full_var}|cut -d= -f1)"
+
+  if [[ "${yq_expr_var}" ]]; then
+    yq_expr_val=$(eval "echo \$YQ_EXPRESSION_${digit}"|sed "s/VERSION_TO_REPLACE/${VERSION}/g")
+    if [[ "${yq_expr_val}" ]]; then
+      export "${yq_expr_var}=${yq_expr_val}"
+    fi
+  fi
+}
+
+yamls_patch() {
+  replace_version "YQ_EXPRESSION=${YQ_EXPRESSION}"
+  env|grep -E "YQ_EXPRESSION_[0-9]+"|while read -r; do
+    replace_version "${REPLY}"
+  done
+  /yaml-patch.sh
+}
+
 if [[ $GIT_BRANCH != "master" && $GIT_BRANCH != "develop" ]] && [[ ! $GIT_BRANCH =~ ^[0-9]+.[0-9]+.x$ ]]; then
   echo "[github-bump-pr] No need to bump because GIT_BRANCH=${GIT_BRANCH}"
   exit 0
@@ -8,7 +28,6 @@ fi
 [[ ! $GIT_TARGET_BRANCH ]] && GIT_TARGET_BRANCH="master"
 
 cd "${TEKTON_WORKSPACE_PATH}"
-/yaml-patch.sh
 
 export VERSION="$(git describe --long|sed "s/-/\./")"
 
@@ -19,6 +38,7 @@ else
   echo "[github-bump-pr] pick VERSION=${VERSION}"
 fi
 
+yamls_patch
 export GIT_SRC_BRANCH="bump_${VERSION}"
 export PR_TITLE="Bump ${REPO_ORG}/${REPO_NAME} to ${VERSION}"
 source /reverse_branches.sh
