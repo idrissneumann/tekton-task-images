a2e3c3d9a4a205fcf1cf5d16f5c8430ddd91e906
Idriss Neumann <idriss.neumann@comwork.io>
2021-01-29 14:37:35 +0100

    Change the wording of logs

--- yarn-checks/yarn-checks.sh
+++ yarn-checks/yarn-checks.sh
@@ -4,8 +4,8 @@
 [ -z "${TARGET}" ] && TARGET="only_install"
 
 if [ "${LOG_LEVEL}" = "debug" ] || [ "${LOG_LEVEL}" = "DEBUG" ]; then
-  echo "[yarn-ci][success][debug] target = ${TARGET}, opt = ${INSTALL_EXTRA_OPT}"
-  echo "[yarn-ci][success][debug] Show the occuped space of workspace"
+  echo "[yarn-check][success][debug] target = ${TARGET}, opt = ${INSTALL_EXTRA_OPT}"
+  echo "[yarn-check][success][debug] Show the occuped space of workspace"
   du -sh "${TEKTON_WORKSPACE_PATH}"
 fi
 
@@ -16,7 +16,7 @@ if [ "${YARN_INSTALL}" = "enabled" ]; then
 fi
 
 if [ $rtn -eq 0 ] && [ "${LOG_LEVEL}" = "debug" ] || [ "${LOG_LEVEL}" = "DEBUG" ]; then
-  echo "[yarn-ci][success][debug] Show the occuped space of workspace"
+  echo "[yarn-check][success][debug] Show the occuped space of workspace"
   du -sh "${TEKTON_WORKSPACE_PATH}"
 fi
 
