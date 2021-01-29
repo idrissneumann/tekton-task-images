f881f3ca62bd85303170d9e1d087d430b90a6d39
Idriss Neumann <idriss.neumann@comwork.io>
2021-01-29 14:36:49 +0100

    Change the wording of logs

--- php-checks/php-checks.sh
+++ php-checks/php-checks.sh
@@ -11,32 +11,32 @@ cd "${TEKTON_WORKSPACE_PATH}"
 [ -z "${ENABLE_PHPUNIT}" ] && ENABLE_PHPUNIT="disabled"
  
 if [ "${LOG_LEVEL}" = "debug" ] || [ "${LOG_LEVEL}" = "DEBUG" ]; then
-  echo "[php-ci][debug] Show the occuped space of workspace"
+  echo "[php-checks][debug] Show the occuped space of workspace"
   du -sh "${TEKTON_WORKSPACE_PATH}"
-  echo "[php-ci][debug] SRC_DIR=${SRC_DIR}, ENABLE_COMPOSER_INSTALL=${ENABLE_COMPOSER_INSTALL}, ENABLE_PHPCS=${ENABLE_PHPCS}, ENABLE_PSALM=${ENABLE_PSALM}, ENABLE_PHPUNIT=${ENABLE_PHPUNIT}"
+  echo "[php-checks][debug] SRC_DIR=${SRC_DIR}, ENABLE_COMPOSER_INSTALL=${ENABLE_COMPOSER_INSTALL}, ENABLE_PHPCS=${ENABLE_PHPCS}, ENABLE_PSALM=${ENABLE_PSALM}, ENABLE_PHPUNIT=${ENABLE_PHPUNIT}"
 fi
 
 rtn=0
 if [ "${ENABLE_COMPOSER_INSTALL}" = "enabled" ]; then
-    echo "[php-ci] run composer install ${INSTALL_EXTRA_ARGS}"
+    echo "[php-checks] run composer install ${INSTALL_EXTRA_ARGS}"
     composer install ${INSTALL_EXTRA_ARGS}
     rtn=$?
 fi
 
 if [ $rtn -eq 0 ] && [ "${ENABLE_PHPCS}" = "enabled" ]; then
-    echo "[php-ci] run ./vendor/bin/phpcs ${SRC_DIR}"
+    echo "[php-checks] run ./vendor/bin/phpcs ${SRC_DIR}"
     ./vendor/bin/phpcs ${SRC_DIR}
     rtn=$?
 fi
 
 if [ $rtn -eq 0 ] && [ "${ENABLE_PSALM}" = "enabled" ]; then
-    echo "[php-ci] run ./vendor/bin/psalm -c=${PSALM_FILE}"
+    echo "[php-checks] run ./vendor/bin/psalm -c=${PSALM_FILE}"
     ./vendor/bin/psalm -c=${PSALM_FILE}
     rtn=$?
 fi
 
 if [ $rtn -eq 0 ] && [ "${ENABLE_PHPUNIT}" = "enabled" ]; then
-    echo "[php-ci] run ./vendor/bin/phpunit"
+    echo "[php-checks] run ./vendor/bin/phpunit"
     ./vendor/bin/phpunit
     rtn=$?
 fi
