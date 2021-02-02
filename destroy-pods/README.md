58c613534009fc09a43a248fb5cad3580fdb17aa
Idriss Neumann <idriss.neumann@comwork.io>
2021-02-02 09:04:47 +0100

    Multi-env implementation

--- destroy-pods/README.md
+++ destroy-pods/README.md
@@ -11,3 +11,4 @@ Task for destroy pods in order to let ArgoCD redeploy them with new images.
 * `KUBE_TOKEN`: kubernetes token
 * `DELETE_JOBS`: delete jobs (`enabled` or `disabled`)
 * `DESTROY_MODE`: `rollout` or `delete`(default is `rollout`)
+* `MULTI_ENV`: multi env `enabled` or `disabled`. If it's enabled, you'll have to set `GIT_BRANCH` and `KUBE_{ENV}_ENV`, `KUBE_{ENV}_URL`, `KUBE_{ENV}_TOKEN` variables for each env.
--- init-kube-config/init-kube-config.sh
+++ init-kube-config/init-kube-config.sh
@@ -3,6 +3,26 @@
 [ ! -d "~/.kube" ] && mkdir ~/.kube
 [ -f "~/.kube/config" ] && cp -f ~/.kube/config ~/.kube/config.old
 
+if [ -z "${MULTI_ENV}" ]; then
+  export MULTI_ENV="disabled"
+fi
+
+if [ "${MULTI_ENV}" = "enabled" ]; then
+  if [ "${GIT_BRANCH}" = "qa" ]; then
+    export KUBE_ENV="${KUBE_QA_ENV}"
+    export KUBE_TOKEN="${KUBE_QA_TOKEN}"
+    export KUBE_URL="${KUBE_QA_URL}"
+  elif [ "${GIT_BRANCH}" = "prod" ]; then
+    export KUBE_ENV="${KUBE_PROD_ENV}"
+    export KUBE_TOKEN="${KUBE_PROD_TOKEN}"
+    export KUBE_URL="${KUBE_PROD_URL}"
+  else
+    export KUBE_ENV="${KUBE_DEV_ENV}"
+    export KUBE_TOKEN="${KUBE_DEV_TOKEN}"
+    export KUBE_URL="${KUBE_DEV_URL}"
+  fi
+fi
+
 echo 'apiVersion: v1
 kind: Config
 clusters:
