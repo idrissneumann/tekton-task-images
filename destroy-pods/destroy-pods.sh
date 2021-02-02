bd1b0951fece62dddaf9106457f01461e664cd68
Idriss Neumann <idriss.neumann@comwork.io>
2021-02-02 11:10:35 +0100

    Fix: rollout command

--- destroy-pods/destroy-pods.sh
+++ destroy-pods/destroy-pods.sh
@@ -1,12 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 /init-kube-config.sh
 
-if [ -z "${DESTROY_MODE}" ]; then 
-  export DESTROY_MODE="rollout"
-fi
+[[ $DESTROY_MODE ]] || export DESTROY_MODE="rollout"
 
-if [ "${GIT_BRANCH}" != "develop" ] && [ "${GIT_BRANCH}" != "master" ] && [ "${GIT_BRANCH}" != "qa" ] && [ "${GIT_BRANCH}" != "prod" ] && ! echo "${GIT_BRANCH}"|grep -E "^[0-9]+.[0-9]+.x$" > /dev/null; then
+if [[ ! $GIT_BRANCH =~ ^([0-9]+.[0-9]+.x|master|develop|main|prod|qa)$ ]]; then
   echo "[destroy-pods] No pod to remove because KUBE_ENV=${KUBE_ENV} and GIT_BRANCH=${GIT_BRANCH}"
   exit 0
 fi
@@ -14,10 +12,19 @@ fi
 echo "[destroy-pods] Destroy pods on namespace ${NAMESPACE} that begins with ${POD_PREFIX} with mode = ${DESTROY_MODE}, delete_jobs = ${DELETE_JOBS}"
 
 kind="deployments"
-if [ "${DESTROY_MODE}" = "delete" ]; then
+cmd=("rollout" "restart" "${kind}")
+if [[ "${DESTROY_MODE}" = "delete" ]]; then
   kind="pods"
+  cmd=("delete" "${kind}")
 fi
 
-kubectl -n "$NAMESPACE" get "${kind}"|grep -E "^${POD_PREFIX}"|awk '{print $1}'|xargs kubectl -n "$NAMESPACE" "${DESTROY_MODE}" "${kind}" || :
-[ "${DELETE_JOBS}" = "true" ] || [ "${DELETE_JOBS}" = "enabled" ] && kubectl -n "${NAMESPACE}" get jobs|grep -E "^${POD_PREFIX}"|awk '{print $1}'|xargs kubectl -n "${NAMESPACE}" delete job || :
+kubectl -n "${NAMESPACE}" get "${kind}"|grep -E "^${POD_PREFIX}"|while read -r res trash; do
+  kubectl -n "${NAMESPACE}" ${cmd[@]} "${res}" || :
+done
+
+if [[ $DELETE_JOBS = "true" || $DELETE_JOBS = "enabled" ]]; then
+  kubectl -n "${NAMESPACE}" get jobs|grep -E "^${POD_PREFIX}"|while read -r res trash; do
+    kubectl -n "${NAMESPACE}" delete job "${res}" || :
+  done
+fi
 [ -f ~/.kube/config.old ] && cp -f ~/.kube/config.old ~/.kube/config
