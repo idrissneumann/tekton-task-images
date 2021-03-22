a4c1b376ba30f864aeb3c5534d3941a51159009e
Idriss Neumann <idriss.neumann@comwork.io>
2021-03-22 14:57:22 +0100

    Add more resilience when hostname is not matching a uuid correctly

--- env-files-utils/env_files_utils.py
+++ env-files-utils/env_files_utils.py
@@ -4,7 +4,11 @@ import re
 def extract_pipeline_id():
     pattern = "[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{8}"
     match = re.search(pattern, os.environ['HOSTNAME'])
-    return match.group(0)
+    if match:
+        return match.group(0)
+    else:
+        print("[extract_pipeline_id] no uid found on this HOSTNAME : {}".format(os.environ['HOSTNAME']))
+        return None
 
 def get_pr_json_file():
     f = "{}/prs-{}.json".format(os.environ['TEKTON_WORKSPACE_PATH'], extract_pipeline_id())
