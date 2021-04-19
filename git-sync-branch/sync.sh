198bc9e9b0224464967a621496d99b5224df3a42
Idriss Neumann <idriss.neumann@comwork.io>
2021-04-19 07:42:50 +0100

    Add git sync-branch image

---
+++ git-sync-branch/sync.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+git checkout $GIT_SRC_BRANCH
+git pull --rebase origin $GIT_SRC_BRANCH
+git checkout $GIT_TARGET_BRANCH
+git pull --rebase origin $GIT_TARGET_BRANCH
+git merge $GIT_SRC_BRANCH
+git push origin $GIT_TARGET_BRANCH
