9b2b11be2c77ae671693fd58e44e9e7c7cb0442f
Idriss Neumann <idriss.neumann@comwork.io>
2021-05-26 07:32:31 +0100

    Add buildkit task

---
+++ buildkit-build/README.md
@@ -0,0 +1,28 @@
+
+# Build Kaniko
+
+Build containers images with Buildkit.
+
+## Environment variables to set
+
+* `LOG_LEVEL`: log level (`debug` or `info`)
+* `GIT_BRANCH`: git branch name
+* `ENABLE_CACHE` (optional): enable build with cache
+* `CACHE_TTL`: cache time to live
+* `REGISTRY`: conatiners images registry
+* `IMAGE`: image name
+* `IMAGE_TAG` (optional): image tag
+* `EXTRA_ARGS` (optional): extra args (multistage targets for example)
+* `DOCKERFILE`: path to the Dockerfile
+* `CONTEXT`: the build context used by Kaniko
+* `PROJECT_STABLE`: path of stables images in the registry (after merge)
+* `PROJECT_UNSTABLE`: path of stables images in the registry (before merge)
+* `FORCE_PROJECT_UNSTABLE`: force push in the `$PROJECT_UNSTABLE` path
+* `TEKTON_WORKSPACE_PATH`: the tekton workspace path which is bind to a pvc
+* `MULTI_ENV` (optional): enabled mutli-env delivery (push the image in a subdir `/prod` or `/qa` if it's `enabled` and if `$GIT_BRANCH` match)
+* `VERSIONING_FROM_TAG` (optional): enable versioning from git tag
+* `RETRY_NUMBER` (optional): number of retry for pushing the images into the registry
+
+## Tekton example
+
+See the [example](./build-container-image.yaml)
---
+++ buildkit-build/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+source /oci-build-utils.sh
+
+export IMAGE="$(get_image_path)"
+echo "$IMAGE" > "$IMAGE_NAME_PERSISTENT_FILE"
+
+echo "[build-container-image] Image to build = ${IMAGE}, extra args = ${EXTRA_ARGS}, multi env = ${MULTI_ENV}, versioning from tag = ${VERSIONING_FROM_TAG}, version from tag = ${DELIVERY_VERSION_FROM_TAG}"
+DEBUG_OPT=""
+[[ ! $BASE_OPTS ]] && export BASE_OPTS="--progress=plain --frontend=dockerfile.v0"
+[[ ! $PUSH_OPTS ]] && export PUSH_OPTS="--output type=image,name=$IMAGE,push=true"
+[[ ! $EXTRA_ARGS ]] && export EXTRA_ARGS=""
+DOCKER_CONTEXT="${TEKTON_WORKSPACE_PATH}/${CONTEXT}"
+
+if [[ $LOG_LEVEL == "debug" || $LOG_LEVEL == "DEBUG" ]]; then
+  ls -la
+  DEBUG_OPT="--debug"
+fi
+
+CACHE_OPTS=""
+if [[ $ENABLE_CACHE == "enabled" || $ENABLE_CACHE == "ok" || $ENABLE_CACHE == "true" || $ENABLE_CACHE == "enabled" ]]; then
+  CACHE_OPTS="--export-cache type=inline --import-cache type=registry,ref=$IMAGE"
+fi
+
+[[ $RETRY_NUMBER =~ ^[0-9]+$ ]] || export RETRY_NUMBER="5"
+
+export BUILDCTL_CONNECT_RETRIES_MAX="${RETRY_NUMBER}"
+
+/usr/bin/buildctl-daemonless.sh $DEBUG_OPT build $BASE_OPTS --opt filename=$DOCKERFILE --local context=$DOCKER_CONTEXT --local dockerfile=$DOCKER_CONTEXT $PUSH_OPTS $CACHE_OPTS $EXTRA_ARGS
+
