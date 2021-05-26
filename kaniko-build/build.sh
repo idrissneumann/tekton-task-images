41ecbd1cf3ef8380bb1bfb4683156ea1a3eb6525
Idriss Neumann <idriss.neumann@comwork.io>
2021-05-26 07:29:08 +0100

    Refactoring in order to re-use some parts

--- kaniko-build/build.sh
+++ kaniko-build/build.sh
@@ -1,48 +1,8 @@
 #!/bin/bash
 
-source /env_files_utils.sh
-pr_env_file="$(get_pr_env_file)"
+source /oci-build-utils.sh
 
-if [[ $LOG_LEVEL == "debug" || $LOG_LEVEL == "DEBUG" ]]; then
-  echo "[build-container-image][debug][pr_env_file] loading the file ${pr_env_file}"
-fi
-
-if [[ -f $pr_env_file ]]; then
-  source "${pr_env_file}"
-else
-  echo "pr_env_file = ${pr_env_file} not exists"
-fi
-
-if [[ $LOG_LEVEL == "debug" || $LOG_LEVEL == "DEBUG" ]]; then
-  echo "[build-container-image][debug] Content of the current directory"
-  ls -la
-fi
-
-[[ $VERSIONING_FROM_TAG ]] || export VERSIONING_FROM_TAG="disabled"
-[[ $MULTI_ENV ]] || export MULTI_ENV="disabled"
-
-DOCKER_REGISTRY_ORG="${PROJECT_STABLE}"
-is_unstable=""
-if [[ ! $GIT_BRANCH =~ ^([0-9]+.[0-9]+.x|master|develop|main|prod|qa|ppd|preprod)$ ]] || [[ $FORCE_PROJECT_UNSTABLE == "enabled" ]]; then
-  DOCKER_REGISTRY_ORG="${PROJECT_UNSTABLE}"
-  is_unstable="true"
-elif [[ $MULTI_ENV == "enabled" && $GIT_BRANCH == "qa" && $VERSIONING_FROM_TAG != "enabled" ]]; then
-  DOCKER_REGISTRY_ORG="${PROJECT_STABLE}/qa"
-elif [[ $MULTI_ENV == "enabled" && $GIT_BRANCH == "prod" && $VERSIONING_FROM_TAG != "enabled" ]]; then
-  DOCKER_REGISTRY_ORG="${PROJECT_STABLE}/prod"
-elif [[ $MULTI_ENV == "enabled" && $GIT_BRANCH =~ ^(ppd|preprod)$ && $VERSIONING_FROM_TAG != "enabled" ]]; then
-  DOCKER_REGISTRY_ORG="${PROJECT_STABLE}/preprod"
-fi
-
-final_tag="latest"
-if [[ $VERSIONING_FROM_TAG == "enabled" && $DELIVERY_VERSION_FROM_TAG && ! $is_unstable ]]; then
-  final_tag="${DELIVERY_VERSION_FROM_TAG}"
-  [[ $IMAGE_TAG && $IMAGE_TAG != "latest" ]] && final_tag="${IMAGE_TAG}-${final_tag}"
-elif [[ $IMAGE_TAG ]]; then
-  final_tag="${IMAGE_TAG}"
-fi
-
-IMAGE="${DOCKER_REGISTRY}/${DOCKER_REGISTRY_ORG}/${IMAGE}:${final_tag}"
+export IMAGE="$(get_image_path)"
 echo "$IMAGE" > "$IMAGE_NAME_PERSISTENT_FILE"
 
 echo "[build-container-image] Image to build = ${IMAGE}, extra args = ${EXTRA_ARGS}, multi env = ${MULTI_ENV}, versioning from tag = ${VERSIONING_FROM_TAG}, version from tag = ${DELIVERY_VERSION_FROM_TAG}"
