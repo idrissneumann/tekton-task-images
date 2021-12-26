4c05e39bf84bc2317fab95ab8f708534569c7503
Idriss Neumann <neumann.idriss@gmail.com>
2021-12-26 21:22:04 +0000

    Update mirror.sh

--- ci/mirror.sh
+++ ci/mirror.sh
@@ -5,5 +5,5 @@ REPO_PATH="/home/centos/tekton-task-images/"
 cd "${REPO_PATH}" && git pull origin master || :
 git push github master 
 git push pgitlab master
-git push pgithub master
+git push internal master
 exit 0
